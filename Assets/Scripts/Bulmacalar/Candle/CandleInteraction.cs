8f715ef75eb90edac631745d88a2fb9f34a2f755
Ahmet Emir Coskun <aecoskuno@gmail.com>
2025-08-02 01:38:29 +0300

    level-2 tamamen bitirildi.

--- Assets/Scripts/Bulmacalar/Candle/CandleInteraction.cs
+++ Assets/Scripts/Bulmacalar/Candle/CandleInteraction.cs
@@ -6,19 +6,42 @@ public class CandleInteraction : MonoBehaviour
     public float interactDistance = 3f;
     public Camera playerCamera; // FPS kameray� buraya Inspector�dan atamal�s�n
 
+    Candle lastCandle = null;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) // E
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
         {
-            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
-            if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
+            Candle candle = hit.collider.GetComponent<Candle>();
+            if (candle != null)
             {
-                Candle candle = hit.collider.GetComponent<Candle>();
-                if (candle != null)
+                // ShowPrompt
+                var prompt = candle.GetComponent<KeyPromptDisplay>();
+                if (prompt != null) prompt.ShowPrompt();
+
+                lastCandle = candle;
+
+                // E tuşu ile yak
+                if (Input.GetKeyDown(KeyCode.E))
                 {
                     candle.Toggle();
                 }
             }
+            else if (lastCandle != null)
+            {
+                // Raycast'ta candle yoksa en son gösterilen promptu gizle
+                var prompt = lastCandle.GetComponent<KeyPromptDisplay>();
+                if (prompt != null) prompt.HidePrompt();
+                lastCandle = null;
+            }
+        }
+        else if (lastCandle != null)
+        {
+            // Raycast hiçbir şeye değmiyorsa, promptu yine gizle
+            var prompt = lastCandle.GetComponent<KeyPromptDisplay>();
+            if (prompt != null) prompt.HidePrompt();
+            lastCandle = null;
         }
     }
 }
